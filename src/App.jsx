d165d90020d399f2ef3c9ce7b29f4f1627df6235
SaqibSattar <saqibkhanz1234@gmail.com>
2024-01-23 11:19:40 +0400

    implement CountUp

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,45 @@
-import { useState } from "react";
-import Tippy from "@tippyjs/react";
-import "tippy.js/dist/tippy.css"; // optional
+import { useRef, useState } from "react";
+import CountUp, { useCountUp } from "react-countup";
 
 function App() {
+  const countUpRef = useRef(null);
+  const { start, pauseResume, reset, update } = useCountUp({
+    ref: countUpRef,
+    start: 0,
+    end: 1234567,
+    delay: 1000,
+    duration: 5,
+    onReset: () => console.log("Resetted!"),
+    onUpdate: () => console.log("Updated!"),
+    onPauseResume: () => console.log("Paused or resumed!"),
+    onStart: ({ pauseResume }) => console.log(pauseResume),
+    onEnd: ({ pauseResume }) => console.log(pauseResume),
+  });
   return (
-    <>
-    <div style={{ paddingBottom: '20px'}}>
-    <Tippy arrow={false} delay={1000} content="Basic Tooltip">
-    <button>Hover me</button>
-    </Tippy>
+    <div className="App">
+      <CountUp end={100} />
+      <br />
+      <CountUp end={100} duration={5} />
+      <br />
+      <CountUp start={500} end={1000} duration={5} />
+      <br />
+      <CountUp
+        start={-875.039}
+        end={160527.012}
+        separator=" "
+        decimals={4}
+        decimal=","
+        prefix="EUR "
+        suffix=" left"
+        onEnd={() => console.log("Ended! 👏")}
+        onStart={() => console.log("Started! 💨")}
+      ></CountUp>
+      <div ref={countUpRef} />
+      <button onClick={start}>Start</button>
+      <button onClick={reset}>Reset</button>
+      <button onClick={pauseResume}>Pause/Resume</button>
+      <button onClick={() => update(2000)}>Update to 2000</button>
     </div>
-    <div style={{ paddingBottom: '20px'}}>
-    <Tippy placement="bottom-start" content={<span style={{ color: "orange" }}>Basic Tooltip with color</span> }>
-    <button>Hover me</button>
-    </Tippy>
-    </div>
-    </>
   );
 }
 
