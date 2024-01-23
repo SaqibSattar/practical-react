4783619c89152afdee446b038dca8ada97a23142
SaqibSattar <saqibkhanz1234@gmail.com>
2024-01-23 10:47:06 +0400

    implement Modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,27 @@
 import { useState } from 'react'
-import { ToastContainer, toast } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
+import Modal from 'react-modal';
+
+
+// Make sure to bind modal to your appElement
+Modal.setAppElement('#root');
 
 function App() {
-  const notify = () => {
-    toast('Default Notification !')
-
-    toast.success('Success Notification !', {
-      position: 'top-center',
-    })
-
-    toast.error('Error Notification !', {
-      position: 'top-left',
-    })
-
-    toast.warn('Warning Notification !', {
-      position: 'bottom-left',
-    })
-
-    toast.info('Info Notification !', {
-      position: 'bottom-center',
-    })
-
-    toast('Custom Style Notification with css class!', {
-      position: 'bottom-right',
-      className: 'foo-bar',
-    })
-  }
+  const [modalIsOpen, setModalIsOpen] = useState(false);
 
   return (
-    <>
-      <button onClick={notify}>Notify</button>;
-      <ToastContainer />
-    </>
-  )
+    <div>
+      <button onClick={() => { setModalIsOpen(true)}}>Open Modal</button>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={() => { setModalIsOpen(false)}}
+        contentLabel="Example Modal"
+      >
+        <h2>Hello</h2>
+        <button onClick={() => { setModalIsOpen(false)}}>close</button>
+        <div>I am a modal</div>
+      </Modal>
+    </div>
+  );
 }
 
 export default App
