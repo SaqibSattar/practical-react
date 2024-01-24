39ae61c59f9d626f058ced0b3d8d2a20f37680e5
SaqibSattar <saqibkhanz1234@gmail.com>
2024-01-24 14:38:17 +0400

    implement credit-cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,67 @@
-import React, { useState } from "react";
-import { ChromePicker, SketchPicker, SwatchesPicker } from "react-color";
+import React, { useState } from 'react';
+import Cards from 'react-credit-cards';
+import 'react-credit-cards/lib/styles.scss';
 
-function App() {
-  const [color, setColor] = useState("#fff");
-  const [secondColor, setSecondColor] = useState("#fff");
-  const [thirdColor, setThirdColor] = useState("#fff");
+const App = () => {
+  const [cardInfo, setCardInfo] = useState({
+    cvc: '',
+    expiry: '',
+    focus: '',
+    name: '',
+    number: '',
+  });
+
+  const handleInputFocus = (e) => {
+    setCardInfo({ ...cardInfo, focus: e.target.name });
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setCardInfo({ ...cardInfo, [name]: value });
+  };
 
   return (
-    <div>
-      <ChromePicker
-        color={color}
-        onChange={(updateColor) => {
-          console.log(updateColor);
-          setColor(updateColor.hex);
-        }}
+    <div id="PaymentForm">
+      <Cards
+        cvc={cardInfo.cvc}
+        expiry={cardInfo.expiry}
+        focused={cardInfo.focus}
+        name={cardInfo.name}
+        number={cardInfo.number}
       />
-      <p>Color one is: {color}</p>
-      <SketchPicker
-        color={secondColor}
-        onChange={(updateColor) => {
-          console.log(updateColor);
-          setSecondColor(updateColor.hex);
-        }}
-      />
-      <p>Color Two is: {secondColor}</p>
-      <SketchPicker
-      color={thirdColor}
-      onChange={(updateColor) => {
-        console.log(updateColor);
-        setThirdColor(updateColor.hex);
-      }}
-    />
-    <p>Color Three is: {thirdColor}</p>
-
+      <form>
+        <input
+          type="tel"
+          name="number"
+          placeholder="Card Number"
+          onChange={handleInputChange}
+          onFocus={handleInputFocus}
+        />
+        <input
+          type="text"
+          name="name"
+          placeholder="Card Holder Name"
+          onChange={handleInputChange}
+          onFocus={handleInputFocus}
+        />
+        <input
+          type="tel"
+          name="expiry"
+          placeholder="MM/YY Expiry"
+          onChange={handleInputChange}
+          onFocus={handleInputFocus}
+        />
+        <input
+          type="tel"
+          name="cvc"
+          placeholder="CVC"
+          onChange={handleInputChange}
+          onFocus={handleInputFocus}
+        />
+        {/* Add other input fields as needed */}
+      </form>
     </div>
   );
-}
+};
 
 export default App;
