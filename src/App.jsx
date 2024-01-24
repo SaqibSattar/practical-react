347f704b7749ae8e46070d56f231ad34b0c698b7
SaqibSattar <saqibkhanz1234@gmail.com>
2024-01-24 14:55:40 +0400

    implement datepicker

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,21 @@
 import React, { useState } from 'react';
-import Cards from 'react-credit-cards';
-import 'react-credit-cards/lib/styles.scss';
+import DatePicker from "react-datepicker";
+
+import "react-datepicker/dist/react-datepicker.css";
 
 const App = () => {
-  const [cardInfo, setCardInfo] = useState({
-    cvc: '',
-    expiry: '',
-    focus: '',
-    name: '',
-    number: '',
-  });
-
-  const handleInputFocus = (e) => {
-    setCardInfo({ ...cardInfo, focus: e.target.name });
-  };
-
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setCardInfo({ ...cardInfo, [name]: value });
-  };
-
+  const [startDate, setStartDate] = useState(new Date());
   return (
-    <div id="PaymentForm">
-      <Cards
-        cvc={cardInfo.cvc}
-        expiry={cardInfo.expiry}
-        focused={cardInfo.focus}
-        name={cardInfo.name}
-        number={cardInfo.number}
-      />
-      <form>
-        <input
-          type="tel"
-          name="number"
-          placeholder="Card Number"
-          onChange={handleInputChange}
-          onFocus={handleInputFocus}
-        />
-        <input
-          type="text"
-          name="name"
-          placeholder="Card Holder Name"
-          onChange={handleInputChange}
-          onFocus={handleInputFocus}
-        />
-        <input
-          type="tel"
-          name="expiry"
-          placeholder="MM/YY Expiry"
-          onChange={handleInputChange}
-          onFocus={handleInputFocus}
-        />
-        <input
-          type="tel"
-          name="cvc"
-          placeholder="CVC"
-          onChange={handleInputChange}
-          onFocus={handleInputFocus}
-        />
-        {/* Add other input fields as needed */}
-      </form>
-    </div>
+    <DatePicker 
+    selected={startDate} 
+    onChange={(date) => setStartDate(date)}
+    dateFormat='MM/dd/yyyy'
+    minDate={new Date()}
+    filterDate={date => date.getDay() !== 6 && date.getDay() !== 0}
+    isClearable={true}
+    showYearDropdown={true}
+    showMonthDropdown={true}
+    />
   );
 };
 
